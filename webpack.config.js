dc22e9a1abc1b0ff328770b279c72888d3a84bd0
mrT <lptra13@gmail.com>
2021-01-04 13:05:57 +0700

    Setup production build an server

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // input
-    entry: ['babel-polyfill', './src/js/index.js'],
-    output: {
-        // path to the folder need to be an absolute path
-        // __dirname: the current absolute path  -> the FORKIFY directory
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-            // babel-loader
-            {
-                test: /\.js$/,  //regular expression
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+	// input
+	entry: ['babel-polyfill', './src/js/index.js'],
+	output: {
+		// path to the folder need to be an absolute path
+		// __dirname: the current absolute path  -> the FORKIFY directory
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
+	},
+	devServer: {
+		contentBase: './dist',
+		historyApiFallback: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	],
+	module: {
+		rules: [
+			// babel-loader
+			{
+				test: /\.js$/, //regular expression
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
